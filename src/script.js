7ae59f70f7cfd2fcfb4dc3eb7685512854f8ff33
ajmeriunnisa <ajmeriunnisa877@gmail.com>
2025-07-30 16:39:22 +0530

    Initialized JS with basic structure and DOM selectors

--- src/script.js
+++ src/script.js
@@ -1,1 +1,14 @@
 
+// OpenWeatherMap API Key
+const API_KEY="7631e2ed1cff814273ba939280f5dda1";
+
+
+//Get DOM elements
+const searchBtn=document.getElementById("searchBtn");
+const currentBtn=document.getElementById("currentBtn");
+const cityInput = document.getElementById("cityInput");
+const errorMsg = document.getElementById("errorMsg");
+const weatherInfo = document.getElementById("weatherInfo");
+const forecastSection = document.getElementById("forecastSection");
+const forecastContainer = document.getElementById("forecastContainer");
+const recentCities = document.getElementById("recentCities");
